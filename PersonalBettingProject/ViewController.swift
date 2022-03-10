ef2b1a2ae529c2eefe2e0e0a20aca72c611422c9
nikoagge <nikos@souniolabs.com>
2022-03-10 09:54:55 +0200

    Initial Commit

---
+++ PersonalBettingProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalBettingProject
+//
+//  Created by Nikos Aggelidis on 10/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
