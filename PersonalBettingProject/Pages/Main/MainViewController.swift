012e6eaca6d6d43e106d00d6275192ff2e31046f
nikoagge <nikos@souniolabs.com>
2022-03-10 21:13:24 +0200

    Basic organization and tidying.

---
+++ PersonalBettingProject/Pages/Main/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewController.swift
+//  PersonalBettingProject
+//
+//  Created by Nikos Aggelidis on 10/3/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()        
+    }
+}
